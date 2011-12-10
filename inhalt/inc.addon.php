cdb3c9bc43edc1b64c211fef4d1d05da3ce1b629
finke <Surf-finke@gmx.de>
2011-12-10 14:02:23 +0100

    noch ne schnelle Korektur vorm Mittagessen

--- inhalt/inc.addon.php
+++ inhalt/inc.addon.php
@@ -1,33 +1,3 @@
 <?php
-/**
-*Startet das Addonsystem
-*
-*
-*@autor finke
-*
-*/
-function init_addon_system(){
-if(defined('EXTENSION')){
-  return false;
-}
-define('EXTENSION', true);
-$path = dirname(__file__).DIRECTORY_SEPARATOR.'extend'.DIRECTORY_SEPARATOR;
-global $extends = array();
-$ext = scandir($path);
-foreach($ext as $dir){
-  if(is_dir($path.$dir) && file_exist($path.$dir.DIRECTORY_SEPARATOR.'include.php')){
-    include $path.$dir.DIRECTORY_SEPARATOR.'include.php';
-	global $extends[] = new $dir;
-  }
-}
-}
-
-/**
-*
-*Wird von den Seiten aufgerufen, wenn ein Event eingetreten ist
-*@autor finke
-*
-*
-*/
-function trip_a_event($name){
-
+require_once(dirname(__FILE__).'/classes/class.addonSystem.php');
+global $addonSystem = new AddonSystem();
