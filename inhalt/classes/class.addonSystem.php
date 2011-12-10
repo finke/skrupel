4ab03909fb56689b30a862ac9a43922a2fdd2ed3
finke <Surf-finke@gmx.de>
2011-12-10 13:49:37 +0100

    OOP von anfang an

---
+++ inhalt/classes/class.addonSystem.php
@@ -0,0 +1,35 @@
+<?php
+class AddonSystem{
+private $_ext_path = dirname(__file__)'/../../extend/';
+private $_extensions = array();
+
+/**
+*Startet das Addonsystem
+*
+*
+*@autor finke
+*
+*/
+  public function __construct(){
+    $ext = scandir($path);
+    foreach($ext as $dir){
+      if(is_dir($path.$dir) && file_exist($path.$dir.'/include.php')){
+      include $path.$dir.'/include.php';
+	  $this->_extensions[] = new $dir;
+    }
+  }
+}
+
+/**
+*
+*Wird von den Seiten aufgerufen, wenn ein Event eingetreten ist
+*@autor finke
+*
+*
+*/
+  public function trip_a_event($name){
+	foreach($this->_extensions as $extension){
+	  $extension->trip_a_event($name);
+	}
+  }
+}
--- inhalt/inc.addon.php
+++ inhalt/inc.addon.php
@@ -1,4 +1,15 @@
 <?php
+/**
+*Startet das Addonsystem
+*
+*
+*@autor finke
+*
+*/
+function init_addon_system(){
+if(defined('EXTENSION')){
+  return false;
+}
 define('EXTENSION', true);
 $path = dirname(__file__).DIRECTORY_SEPARATOR.'extend'.DIRECTORY_SEPARATOR;
 global $extends = array();
@@ -9,4 +20,14 @@ foreach($ext as $dir){
 	global $extends[] = new $dir;
   }
 }
+}
+
+/**
+*
+*Wird von den Seiten aufgerufen, wenn ein Event eingetreten ist
+*@autor finke
+*
+*
+*/
+function trip_a_event($name){
 
