5800eba158cbc5a12a4fb31d0ceb2ec0b57510c9
finke <Surf-finke@gmx.de>
2011-12-10 22:10:52 +0100

    blubb

--- myAddon/index.php
+++ myAddon/index.php
@@ -1,6 +1,15 @@
 <?php
 defined('EXTENSION') or die();
 require_once 'inhalt/classes/class.skrupelAddon.php';
-class myAddon extends SkrupelAddon{
+class MyAddon extends SkrupelAddon{
 
+	public function __construct(){
+		//Arbeiten, die beim initialisieren der Sitzung noetig sind.
+	}
+	public function __destruct() {
+		//Aufraemarbeiten beim beenden des Seitenaufrufes
+	}
+	public function trip_event($name){
+		//Hir kommt dann rein, was passiert, wenn ein bestimtes event (Zugende, spielende...) eingetretenist
+	}
 }
