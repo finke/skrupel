4f2c0dfcf50ce2499abdd83020ece083544c7614
finke <Surf-finke@gmx.de>
2011-12-18 00:28:47 +0100

    Mein Vorschlag zur Uebersichtlichkeit der Abbonklasse

--- myAddon/index.php
+++ myAddon/index.php
@@ -10,6 +10,21 @@ class MyAddon extends SkrupelAddon{
 		//Aufraemarbeiten beim beenden des Seitenaufrufes
 	}
 	public function trip_event($name){
-		//Hir kommt dann rein, was passiert, wenn ein bestimtes event (Zugende, spielende...) eingetretenist
+		//Hir kommt dann rein, was passiert, wenn ein bestimtes event (Zugende,spielende...) eingetretenist
+		//Mien Vorschlag wegs Uebersichtlcihkeit, ein Swichtch, welcher die relevanten Events abfaengt
+		// und je eine private Funktion, welche sie verarbeitet
+		switch($name){
+			case PLAYER_DIE:
+				return $this->player_die();
+			break;
+			case ROUND_END:
+				return $this->round_end();
+			break;
+		}
+	}
+	
+	private function player_die(){
+	}
+	private function round_end(){
 	}
 }
