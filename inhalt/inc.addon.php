46b5f01bab47e1eb9513cc617f392b4856e9af20
finke <Surf-finke@gmx.de>
2011-12-18 00:35:09 +0100

    erster Vorschlag zu Triggern

--- inhalt/inc.addon.php
+++ inhalt/inc.addon.php
@@ -1,3 +1,8 @@
 <?php
+define('ROUND_END', 'round_end');
+define('PLAYER_DIE', 'player_die');
+
+
 require_once(dirname(__FILE__).'/classes/class.addonSystem.php');
 global $addonSystem = new AddonSystem();
+
